efd384f578e8e692f8d59d3673efbfd4bb77bef2
ReiszD <reiszdavis@gmail.com>
2025-08-03 16:37:20 -0400

    latest update

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 // OMDB API KEY: https://www.omdbapi.com/?i=tt3896198&apikey=253f9b44
 // OMDB API: https://www.omdbapi.com/?apikey=253f9b44&s=avengers
 
-async function main() {
+async function renderMedia(filter) {
    const avengers = await fetch("https://www.omdbapi.com/?apikey=253f9b44&s=avengers");
    const avengersData = await avengers.json();
-   console.log(
-    avengersData.map((avengers) =>
-    `<div class="avengers__card">
-        <div class="avengers__card--container">
-            <h2>${Title}</h2>
-            <p><b>Type:</b> ${Type}</p>
-            <p><b>Year:</b> ${Year}</p>
-            <p><b>Poster</b> <a href="https://website.website"></a></p>
-        </div>
-    </div>`)
-   );
+   const avengersListEl = document.querySelector('.avengers__list');
+   if (filter === 'OLD_TO_NEW') {
+       console.log(filter)
+        const renderedMedia = avengers.sortByYear.sort((a, b) => (a.Year) - (b.Year));
+        console.log(renderedMedia)
+    }
+    
+    avengersListEl.innerHTML = avengersData.Search.map((avengers) => avengersHTML(avengers)).join("");
 }
-
-main();
+    
+    function avengersHTML(avengers) {
+        return `<div class="avengers__card">
+        <div class="avengers__card--container">
+        <h2>${avengers.Title}</h2>
+        <p><b>Type:</b> ${avengers.Type}</p>
+        <p><b>Year:</b> ${avengers.Year}</p>
+        <p><b>Poster</b> <img scr="${avengers.Poster}" target="_blank">${avengers.Poster}></p>
+        </div>
+        </div>`
+    }
+    
+    function filterMedia(event) {
+        renderMedia(event.target.value);
+    }
+    
+renderMedia();
